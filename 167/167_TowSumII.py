4262e9fc1fd3aacd14554c10c2bc2f732460ce9a
pchen12567 <pchen12@stevens.edu>
2019-06-25 13:40:57 -0400

    Upload #167

---
+++ 167/167_TowSumII.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Solution 1
+        # Time: 44ms
+        dic = {}
+        for i, n in enumerate(numbers):
+            if target - n in dic:
+                return [dic[target - n], i + 1]
+            else:
+                dic[n] = i + 1
+
+        # Solution 2
+        # Time: 40ms
+        start = 0
+        end = len(numbers) - 1
+        sum = 0
+        while start != end:
+            sum = numbers[start] + numbers[end]
+            if sum > target:
+                end -= 1
+            elif sum < target:
+                start += 1
+            else:
+                return [start + 1, end + 1]
