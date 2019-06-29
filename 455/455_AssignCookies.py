b6dd313d1abc0b65701cc79054253e83e80c99c9
pchen12567 <pchen12@stevens.edu>
2019-06-29 19:02:58 -0400

    Upload #455

---
+++ 455/455_AssignCookies.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+
+        # 贪心问题
+        # Time: 140ms
+        g.sort()
+        s.sort()
+
+        child = 0
+        cookie = 0
+
+        while child < len(g) and cookie < len(s):
+            if g[child] <= s[cookie]:
+                child += 1
+            cookie += 1
+        return child
