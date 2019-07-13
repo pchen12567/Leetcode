7edf0db491dc967ae6a1d02ff9f5c375c7d1183d
pchen12567 <pchen12@stevens.edu>
2019-07-13 17:28:46 -0400

    Upload #409

---
+++ 409/409_LongestPalindrome.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        # 字符串问题
+        # Solution: O(n), O(1)
+        # Time: 20ms
+        from collections import defaultdict
+
+        dic = defaultdict(int)
+
+        for c in s:
+            dic[c] += 1
+
+        ans = 0
+
+        for value in dic.values():
+            ans += value // 2 * 2
+
+            if ans % 2 == 0 and value % 2 == 1:
+                ans += 1
+
+        return ans
