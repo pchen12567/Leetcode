062f6f35e3dd10a225e98ff67e174ec4dcc78c5b
pchen12567 <pchen12@stevens.edu>
2019-08-26 18:20:04 +0800

    Update

--- Mock/百度2017春/不等式数列/Permute.py
+++ Mock/百度2017春/不等式数列/Permute.py
@@ -7,7 +7,7 @@
 """
 
 
-def permute(n, k):
+def permute_s1(n, k):
     cur_d = {i: 1 for i in range(n - k)}
     print(cur_d)
 
@@ -20,10 +20,27 @@ def permute(n, k):
     return cur_d[n - k - 1] % 2017
 
 
+def permute_s2(n, k):
+    import numpy as np
+    dp = np.zeros((n + 1, k + 1))
+    # dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        dp[i][0] = 1
+
+    for i in range(2, n + 1):
+        for j in range(1, k + 1):
+            dp[i][j] = dp[i - 1][j - 1] * (i - j) + dp[i - 1][j] * (j + 1)
+
+    return dp[n][k] % 2017
+
+
 def main():
     n, k = [int(x) for x in input().strip().split()]
-    res = permute(n, k)
-    print(res)
+    res_1 = int(permute_s1(n, k))
+    res_2 = int(permute_s2(n, k))
+    print(res_1)
+    print(res_2)
 
 
 if __name__ == '__main__':
