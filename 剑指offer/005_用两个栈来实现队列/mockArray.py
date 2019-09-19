4f1f45317a9193759320dd84725f8938a2230da4
pchen12567 <pchen12@stevens.edu>
2019-09-20 01:22:28 +0800

    Upload 用两个栈来实现队列

---
+++ 剑指offer/005_用两个栈来实现队列/mockArray.py
@@ -0,0 +1,24 @@
+"""
+@Coding: uft-8
+@Time: 2019-09-20 01:15
+@Author: Ryne Chen
+@File: mockArray.py 
+@Python Version: 3
+"""
+
+
+# -*- coding:utf-8 -*-
+class Solution:
+    def __init__(self):
+        self.stack_1 = []
+        self.stack_2 = []
+
+    def push(self, node):
+        self.stack_1.append(node)
+
+    def pop(self):
+        if not self.stack_2:
+            while self.stack_1:
+                self.stack_2.append(self.stack_1.pop())
+            return self.stack_2.pop()
+        return self.stack_2.pop()
